625eedc62e34743fae03e849238aec6b2d8fef47
thewayifeel93 <thewayifeel93@gmail.com>
2018-02-05 12:42:38 +0900

    sum 함수 추가

--- calculator.java
+++ calculator.java
@@ -2,5 +2,11 @@ package com.mycompany.text;
 
 public class Calculator{
   public static void main(String[] args){
+    int a=4;
+    int b=2;
+    System.out.printIn(sum(a,b));
+  }
+  private static int sum(int a, int b){
+    return a+b;
   }
 }
--- calcultor.py
+++ calcultor.py
@@ -1,6 +1,7 @@
 def summation(a,b):
     return a+b
 
-if __name__ is main:
-    a=[]
-    a.append(int(input("숫자를 입력하세요 :")))
+a=[0,0]
+a[0]=int(input("숫자를 입력하세요 :"))
+a[1]=int(input("숫자를 입력하세요 :"))
+print("합은 %d 입니다."%summation(a[0],a[1]))
