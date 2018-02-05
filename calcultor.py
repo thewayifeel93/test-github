6045d7a2fb416d80b89299649433c16991abf4d3
thewayifeel93 <thewayifeel93@gmail.com>
2018-02-05 12:47:55 +0900

    py 마이너스 추가

--- calcultor.py
+++ calcultor.py
@@ -1,7 +1,15 @@
 def summation(a,b):
     return a+b
+def minus(a,b):
+    return a-b
 
 a=[0,0]
+op=""
 a[0]=int(input("숫자를 입력하세요 :"))
 a[1]=int(input("숫자를 입력하세요 :"))
-print("합은 %d 입니다."%summation(a[0],a[1]))
+op=input("무엇을 하시겠습니까?(더하기/빼기):")
+
+if op=="더하기":
+    print("합은 %d 입니다."%summation(a[0],a[1]))
+if op=="빼기":
+    print("차는 %d 입니다."%minus(a[0],a[1]))
